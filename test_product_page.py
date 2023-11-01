be9be19d68d9c4654e6c3de2503b0e1777fe15af
Svetlana <KalatinaSveta09@yandex.ru>
2023-11-01 13:52:15 +0300

    Доб.тесты в test_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -1,3 +1,22 @@
+from pages.product_page import ProductPage
 
 
 # ФАЙЛ С ТЕСТАМИ ДЛЯ СТРАНИЦЫ ТОВАРА
+
+def test_guest_can_add_product_in_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    page = ProductPage(browser, link)
+    page.open()
+    page.product_add_to_basket()
+    page.solve_quiz_and_get_code()
+    page.should_be_message_about_add_to_basket()
+    page.true_name_product_in_message_about_add_to_basket()
+    page.should_be_message_about_price_basket()
+    page.true_price_basket()
+
+
+def test_should_be_add_to_basket(browser):
+    link = 'http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_add_to_basket()
