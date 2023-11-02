c21095f705cb49026d2f948def50f654cb079661
Svetlana <KalatinaSveta09@yandex.ru>
2023-11-02 16:15:44 +0300

    Группировка тестов логина

--- test_main_page.py
+++ test_main_page.py
@@ -3,28 +3,30 @@ from pages.login_page import LoginPage
 from pages.basket_page import BasketPage
 import pytest
 
+
 # ФАЙЛ С ТЕСТАМИ ДЛЯ ГЛАВНОЙ СТРАНИЦЫ
 
 link = "http://selenium1py.pythonanywhere.com/"
 
 
-def test_guest_can_go_to_login_page(browser):
-    """Тест, что с главной страницы
-    по кнопке для входа переходим по корректному url адресу, где
-    есть форма логина и
-    есть форма регистрации"""
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)  # создаем объект нужного класса с актуальным url
-    login_page.should_be_login_page()
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
+    def test_guest_can_go_to_login_page(self, browser):
+        """Тест, что с главной страницы
+        по кнопке для входа переходим по корректному url адресу, где
+        есть форма логина и
+        есть форма регистрации"""
+        page = MainPage(browser, link)
+        page.open()
+        page.go_to_login_page()
+        login_page = LoginPage(browser, browser.current_url)  # создаем объект нужного класса с актуальным url
+        login_page.should_be_login_page()
 
-
-def test_guest_should_see_login_link(browser):
-    """Тест на наличие кнопки для входа"""
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
+    def test_guest_should_see_login_link(self, browser):
+        """Тест на наличие кнопки для входа"""
+        page = MainPage(browser, link)
+        page.open()
+        page.should_be_login_link()
 
 
 def test_guest_cant_see_product_in_basket_opened_from_main_page(browser):
